b23aff8402b8abbff290a4fc7b2625a394a1400e
mittanmayank <97217764+mittanmayank@users.noreply.github.com>
2022-02-15 15:30:42 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "region" {}
+variable "environment_name" {}
+variable "project_name" {}
+variable "cidr" {}
+variable "subnet_cidr" {}
+variable "subnet_priv_cidr" {}
+variable "myip" {}
+variable "zone" {}
+~
